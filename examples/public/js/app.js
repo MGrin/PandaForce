825341c0d1a717c0b3417a3ffb01dfe5a36cb26d
MGrin <mr6r1n@gmail.com>
2016-12-06 17:31:53 +0100

    Remove useless stuff

--- examples/public/js/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PandaForce from '../../../index.js'
-
-ReactDOM.render(
-  <PandaForce />,
-  document.getElementById('App')
-);
